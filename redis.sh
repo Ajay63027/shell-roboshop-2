502877243fa3fc202293730c6cafdf6d1b21d0ca
Ajay <ajaylearner01@gmail.com>
2025-05-22 13:02:38 +0530

    class19

--- redis.sh
+++ redis.sh
@@ -1,39 +1,7 @@
 #!/bin/bash
-START_TIME=$(date +%s)
-uid=$(id -u)
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-logfolder="/var/log/shell_scriptlogs"
-script_name=$(echo $0 | cut -d "." -f1)
-logfile="$logfolder/$script_name.log"
-
-packages=("nginx" "python3" "mysql" "httpd")
-
-
-mkdir -p $logfolder
-
-echo "script starting at $(date)" | tee -a $logfile
-
-if [ $uid -ne 0 ]
-then
-  echo "ERROR:: user does not have permisions to install" &>>$logfile
-  exit 1
-else
-  echo "user  have permissions to install" &>>$logfile
-fi
-
-VALIDATE(){
-    if [ $1 -eq 0 ];
-  then
-    echo -e "$2   $G successfully $N"  | tee -a $logfile
-  else 
-    echo -e "$2  $R failed $N"  | tee -a $logfile
-    exit 1
-   fi
-}
+source ./common.sh
+app_name=redis
+check_root
 
 dnf module disable redis -y &>>$logfile
 VALIDATE $? "disabling redis"
@@ -50,6 +18,4 @@ VALIDATE $? "opening ip to all and chnaging protected mode"
 systemctl enable redis 
 systemctl start redis 
 VALIDATE $? "enabling and starting redis"
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-echo "total time taken : $TOTAL_TIME seconds"
+check_time
