a6043105bb3828ce973a2ed77f7ffd10f688ab52
Ajay <ajaylearner01@gmail.com>
2025-05-22 12:03:31 +0530

    class19

--- route53.sh
+++ route53.sh
@@ -6,7 +6,7 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z041894539JBJUU4ES2BO"
 DOMAIN_NAME="ajay6.space"
 
-for instance in ${INSTANCES[@]}
+for instance in $@
  do 
   INSTANCEID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0a7153f8526f29a8e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
    if [ $instance != frontend ]
