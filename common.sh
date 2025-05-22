ba52fb0cc26879dad409cb38775137336e89ae15
Ajay <ajaylearner01@gmail.com>
2025-05-22 12:34:48 +0530

    class19

--- common.sh
+++ common.sh
@@ -15,7 +15,7 @@ mkdir -p $logfolder
 
 echo "script starting at $(date)" | tee -a $logfile
 
-check_user(){
+check_root(){
    if [ $uid -ne 0 ]
    then
        echo "ERROR:: user does not have permisions to install" &>>$logfile
@@ -36,6 +36,56 @@ VALIDATE(){
     fi
 }
 
+app_setup(){
+      
+    id roboshop
+    if [ $? -ne 0 ];
+    then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$logfile
+        VALIDATE $? "creating a roboshop user"
+     else 
+     echo -e "user already exist $Y Skipping this part $N"
+     fi
+
+     mkdir -p /app 
+     VALIDATE $? "creating app folder"
+
+     curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$logfile
+     VALIDATE $? "downloading $app_name"
+
+     rm -rf /app/*
+     cd /app 
+     VALIDATE $? "moved to app folder"
+
+
+     unzip /tmp/$app_name.zip &>>$logfile
+     VALIDATE $? "unziping $app_name folder"
+}
+nodejs_setup(){
+    dnf module disable nodejs -y &>>$logfile
+    VALIDATE $? "disabling nodejs"
+
+    dnf module enable nodejs:20 -y &>>$logfile
+    VALIDATE $? "enabling nodejs"
+
+    dnf install nodejs -y &>>$logfile
+    VALIDATE $? "installing nodejs"
+
+    npm install &>>$logfile
+    VALIDATE $? "installing packages"
+}
+sytemd_setup(){
+    cp $script_dir/$app_name.service.repo /etc/systemd/system/$app_name.service
+     VALIDATE $? "creating syctlservices"
+
+     systemctl daemon-reload
+     VALIDATE $? "demonreload"
+
+     systemctl enable $app_name 
+     systemctl start $app_name
+     VALIDATE $? "enable and start"
+
+}
 check_time(){
     END_TIME=$(date +%s)
     TOTAL_TIME=$(( $END_TIME - $START_TIME ))
